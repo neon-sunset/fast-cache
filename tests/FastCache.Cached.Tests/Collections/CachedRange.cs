35cd282a9f7309a0e23f4e7fda08558073a4e92c
neon-sunset <neon_sunset@outlook.com>
2022-06-27 23:43:42 +0300

    Add array pairs test

--- tests/FastCache.Cached.Tests/Collections/CachedRange.cs
+++ tests/FastCache.Cached.Tests/Collections/CachedRange.cs
@@ -20,6 +20,36 @@ public sealed class CachedRangeTests
     }
 
     [Fact]
+    public void SaveRange_InsertedArrayPairs_MatchDataInCache()
+    {
+        var array = GenerateArray();
+        var keys = array.Select(kvp => kvp.Item1).ToArray();
+        var values = array.Select(kvp => kvp.Item2).ToArray();
+        CachedRange.Save(keys, values, TimeSpan.FromHours(1));
+
+        foreach (var (key, expectedValue) in array)
+        {
+            var found = Cached<string>.TryGet(key, out var cached);
+
+            Assert.True(found);
+            Assert.Equal(expectedValue, cached.Value);
+        }
+    }
+
+    [Fact]
+    public void SaveRange_InsertArrayPairs_Throw_OnLengthMismatch()
+    {
+        static void SaveMismatched()
+        {
+            var keys = Enumerable.Range(0, 64).ToArray();
+            var values = Enumerable.Range(0, 32).ToArray();
+            CachedRange.Save(keys, values, TimeSpan.FromHours(1));
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(SaveMismatched);
+    }
+
+    [Fact]
     public void SaveRange_InsertedList_MatchesDataInCache()
     {
         var list = GenerateList();
