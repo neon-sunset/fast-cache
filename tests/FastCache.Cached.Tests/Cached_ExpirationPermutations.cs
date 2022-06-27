9352e28ce5827aeb6de0b6a6032218f73ab6d31e
neon-sunset <neon_sunset@outlook.com>
2022-06-27 22:37:30 +0300

    Add expiration test, fix unnecessary 0 + in Trim() input validation

--- tests/FastCache.Cached.Tests/Cached_ExpirationPermutations.cs
+++ tests/FastCache.Cached.Tests/Cached_ExpirationPermutations.cs
@@ -1,5 +1,27 @@
+using FastCache.Extensions;
+
 namespace FastCache.Cached.Tests;
 
 public sealed class Cached_ExpirationPermutations
 {
+    [Theory]
+    [InlineData(5)]
+    [InlineData(250)]
+    [InlineData(1000)]
+    [InlineData(5000)]
+    public async Task Cached_TryGet_ReturnsValueAccordingToItemExpiration(int milliseconds)
+    {
+        var key = RandomString();
+        var value = RandomString();
+        value.Cache(key, TimeSpan.FromMilliseconds(milliseconds));
+
+        var foundBefore = Cached<string>.TryGet(key, out var cachedBefore);
+        await Task.Delay(milliseconds + 1);
+        var foundAfter = Cached<string>.TryGet(key, out _);
+
+        Assert.True(foundBefore);
+        Assert.Equal(value, cachedBefore.Value);
+
+        Assert.False(foundAfter);
+    }
 }
