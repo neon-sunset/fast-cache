1a68cc4815d3313853df4f24416fbe720888ed94
neon-sunset <neon_sunset@outlook.com>
2022-06-28 14:23:30 +0300

    Reduce expiration test sensitivity to thread preemption and test against higher exp. precision at the same time

--- tests/FastCache.CachedTests/Cached_ExpirationPermutations.cs
+++ tests/FastCache.CachedTests/Cached_ExpirationPermutations.cs
@@ -5,19 +5,20 @@ namespace FastCache.CachedTests;
 public sealed class CachedTests_ExpirationPermutations
 {
     [Theory]
-    [InlineData(100)]
+    [InlineData(10)]
+    [InlineData(250)]
     [InlineData(500)]
     [InlineData(1000)]
     [InlineData(2500)]
-    public async Task Cached_TryGet_ReturnsValueAccordingToItemExpiration(int milliseconds)
+    public async Task Cached_TryGet_ReturnsValueBeforeExpiration_DoesNotReturnAfter(int milliseconds)
     {
-        const int delayTolerance = 50;
+        const int delayTolerance = 100;
 
         var key = GetTestKey(milliseconds);
         var value = GetRandomString();
         value.Cache(key, TimeSpan.FromMilliseconds(milliseconds));
 
-        await Task.Delay(milliseconds - delayTolerance);
+        await Task.Delay(Math.Max(0, milliseconds - delayTolerance));
         var foundBefore = Cached<string>.TryGet(key, out var cachedBefore);
 
         Assert.True(foundBefore);
