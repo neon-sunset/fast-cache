c770c2c31caaffbae78980c735ddaf44a739b620
neon-sunset <neon_sunset@outlook.com>
2022-07-03 20:14:27 +0300

    More eviction tests

--- tests/FastCache.CachedTests/Internals/CacheManager.cs
+++ tests/FastCache.CachedTests/Internals/CacheManager.cs
@@ -1,9 +1,14 @@
+using FastCache.Collections;
+using FastCache.Extensions;
 using FastCache.Services;
 
 namespace FastCache.CachedTests.Internals;
 
 public sealed class CacheManagerTests
 {
+    private record ExpiredEntry(string Value);
+    private record OptionallyExpiredEntry(string Value, bool IsExpired);
+
     [Theory]
     [InlineData(double.NaN)]
     [InlineData(double.MinValue)]
@@ -18,5 +23,62 @@ public sealed class CacheManagerTests
         void Trim() => CacheManager.Trim<int, object>(percentage);
 
         Assert.Throws<ArgumentOutOfRangeException>(Trim);
+    }
+
+    [Fact]
+    public async Task QueueFullEviction_CorrectlyEvictsEntries_AllExpired()
+    {
+        var quickList = CacheStaticHolder<int, ExpiredEntry>.QuickList;
+        var store = CacheStaticHolder<int, ExpiredEntry>.Store;
+
+        var length = Constants.QuickListMinLength * 2;
+        var entries = Enumerable.Range(0, length).Select(i => (i, new ExpiredEntry(GetRandomString())));
+
+        CachedRange<ExpiredEntry>.Save(entries, TimeSpan.FromMilliseconds(250));
+
+        Assert.Equal((uint)Constants.QuickListMinLength, quickList.AtomicCount);
+        Assert.Equal(length, store.Count);
+
+        await Task.Delay(250);
+
+        CacheManager.QueueFullEviction<int, ExpiredEntry>();
+
+        await Task.Delay(50);
+
+        Assert.Equal(0u, quickList.AtomicCount);
+        Assert.Empty(store);
+    }
+
+    [Fact]
+    public async Task QueueFullEviction_CorrectlyEvictsEntries_SomeExpired()
+    {
+        var quickList = CacheStaticHolder<int, OptionallyExpiredEntry>.QuickList;
+        var store = CacheStaticHolder<int, OptionallyExpiredEntry>.Store;
+
+        var length = Constants.QuickListMinLength * 2;
+
+        for (var i = 0; i < length; i++)
+        {
+            _ = i % 2 is 0
+                ? new OptionallyExpiredEntry(GetRandomString(), IsExpired: true).Cache(i, TimeSpan.FromMilliseconds(250))
+                : new OptionallyExpiredEntry(GetRandomString(), IsExpired: false).Cache(i, TimeSpan.MaxValue);
+        }
+
+        Assert.Equal((uint)Constants.QuickListMinLength, quickList.AtomicCount);
+        Assert.Equal(length, store.Count);
+
+        await Task.Delay(250);
+
+        CacheManager.QueueFullEviction<int, OptionallyExpiredEntry>();
+
+        await Task.Delay(50);
+
+        Assert.Equal((uint)length / 2, quickList.AtomicCount);
+        Assert.Equal(length / 2, store.Count);
+
+        foreach (var (_, inner) in store)
+        {
+            Assert.False(inner.Value.IsExpired);
+        }
     }
 }
