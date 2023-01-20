4dd172191c9d99dc13b422f920c5dbdf65724c43
neon-sunset <20912188+neon-sunset@users.noreply.github.com>
2023-01-20 17:50:51 +0200

    Another commit on trying to stabilize tests that verify expiration because of test runner side effects

--- tests/FastCache.CachedTests/Cached_ExpirationPermutations.cs
+++ tests/FastCache.CachedTests/Cached_ExpirationPermutations.cs
@@ -12,19 +12,21 @@ public sealed class CachedTests_ExpirationPermutations
     [InlineData(2500)]
     public async Task Cached_TryGet_ReturnsValueBeforeExpiration_DoesNotReturnAfter(int milliseconds)
     {
-        const int delayTolerance = 100;
+        const int delayTolerance = 200;
+
+        var assertDelay = Math.Max(0, milliseconds - delayTolerance);
 
         var key = GetTestKey(milliseconds);
         var value = GetRandomString();
         value.Cache(key, TimeSpan.FromMilliseconds(milliseconds));
 
-        await Task.Delay(Math.Max(0, milliseconds - delayTolerance));
+        await Task.Delay(assertDelay);
         var foundBefore = Cached<string>.TryGet(key, out var cachedBefore);
 
         Assert.True(foundBefore);
         Assert.Equal(value, cachedBefore);
 
-        await Task.Delay(delayTolerance + 1);
+        await Task.Delay(milliseconds - assertDelay + 1);
         var foundAfter = Cached<string>.TryGet(key, out _);
 
         Assert.False(foundAfter);
